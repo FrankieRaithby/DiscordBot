a74d37b6a2a9823b7b61756e34e0dce0161c45ab
Frankie Raithby <frankieraithby@gmail.com>
2025-03-09 13:34:43 +0000

    Add Discord.py Library

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+import discord
+from discord import Embed, app_commands
 
+MY_GUILD = discord.Object(id=1233757789126398083)
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        # A CommandTree is a special type that holds all the application command
+        # state required to make it work. This is a separate class because it
+        # allows all the extra state to be opt-in.
+        # Whenever you want to work with application commands, your tree is used
+        # to store and work with them.
+        # Note: When using commands.Bot instead of discord.Client, the bot will
+        # maintain its own tree instead.
+        self.tree = app_commands.CommandTree(self)
+
+    # In this basic example, we just synchronize the app commands to one guild.
+    # Instead of specifying a guild to every command, we copy over our global commands instead.
+    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
+
+# logging in
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print('------')
+
+    client.run("MTI5MTQxNjQxNDA5NTE1MTE4NQ.GfHNFL.9yDjejJYHP9y_ev6_QBggHVu7hWWIt5VIqx_zc")
